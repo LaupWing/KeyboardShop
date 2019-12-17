820a8e5dd205457333a3ebe575df999d5ba9716a
LaupWing <loc_strict@hotmail.com>
2019-12-17 20:10:27 +0100

    UPdate frontend

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav id="NavBar">
+
+    </nav>
+</template>
+<script>
+export default {
+    name: 'NavBar',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style>
+#NavBar{
+    width: 100%;
+    background: orange;
+    height: 100px;
+}
+</style>
